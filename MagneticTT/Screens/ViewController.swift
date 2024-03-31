75e804db49c9c020b1c29d3632fca923864216df
Dima Protsak <protsack.dima25@gmail.com>
2024-03-31 20:59:44 +0300

    Base setup

---
+++ MagneticTT/Screens/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MagneticTT
+//
+//  Created by Дмитрий Процак on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
